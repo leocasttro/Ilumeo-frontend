5e294bea844cbb1aa09ee1222a84ba788cd087cc
leocasttro <leocasttro01@icloud.com>
2025-07-08 15:40:45 -0300

    refactor(ActionButton) alteracao do component para reutilizar

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ActionButtonProps {
+  label: string;
+  onClick: () => void | Promise<void>;
+  disabled?: boolean;
+}
+
+const Button = styled.button`
+  width: 108%;
+  padding: 1rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background-color: #FE8A00;
+  color: #151F2B;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+export const ActionButton: React.FC<ActionButtonProps> = ({ label, onClick, disabled = false }) => {
+  return (
+    <Button onClick={onClick} disabled={disabled}>
+      {label}
+    </Button>
+  );
+};
